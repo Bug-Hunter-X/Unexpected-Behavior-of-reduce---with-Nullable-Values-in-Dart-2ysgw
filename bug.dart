3a250780e64694cd756da3f19c005469f8acdb8d
BugHunter-X <askzenai@gmail.com>
2025-01-10 03:52:05 +0100

    Add bug.dart file

---
+++ bug.dart
@@ -0,0 +1,12 @@
+```dart
+List<int> numbers = [1, 2, 3, 4, 5];
+int sum = numbers.reduce((a, b) => a + b);
+print(sum); // Output: 15
+
+//This works fine
+
+List<int?> nullableNumbers = [1, 2, null, 4, 5];
+int? nullableSum = nullableNumbers.reduce((a, b) => a! + b!); //Error occurs here
+print(nullableSum); // Output: Error: Unhandled exception:
+//Null check operator used on a null value
+```
